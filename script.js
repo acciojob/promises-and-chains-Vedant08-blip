19c6701c658aecdffe536e2b38cd600a757dbb02
Vedant08-blip <vedanttrivedi87@gmail.com>
2025-06-19 10:37:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 //your JS code here. If required.
+document.getElementById("votingForm").addEventListener("submit", function (e) {
+  e.preventDefault(); // Prevent form refresh
+
+  const name = document.getElementById("name").value.trim();
+  const age = document.getElementById("age").value.trim();
+
+  if (!name || !age) {
+    alert("Please enter valid details.");
+    return;
+  }
+
+  // Convert age to number
+  const ageNumber = Number(age);
+
+  checkEligibility(name, ageNumber)
+    .then((message) => alert(message))
+    .catch((error) => alert(error));
+});
+
+function checkEligibility(name, age) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (age >= 18) {
+        resolve(`Welcome, ${name}. You can vote.`);
+      } else {
+        reject(`Oh sorry ${name}. You aren't old enough.`);
+      }
+    }, 4000);
+  });
+}
